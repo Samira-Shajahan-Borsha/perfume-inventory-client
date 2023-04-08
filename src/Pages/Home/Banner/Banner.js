78fe0c9285ddf0393cc28f71ecaa06726d176e33
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-04-08 21:38:02 +0600

    added responsive banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import backgroundImage from '../../../Assets/Background_Image/background.jpg';
+import perfumeImage from '../../../Assets/Background_Image/perfume.png';
+import '../Banner/Banner.css';
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <div className='carousel-image w-full'>
+                    <img src={backgroundImage} className="w-full max-h-screen" alt='' />
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-10 right-0 top-1/2">
+                    <div className='lg:mt-20'>
+                        <h2 className='font-bold text-2xl md:text-5xl lg:text-5xl italic text-white text-start '>Everything's better <br /> with  a bit of fragrance. </h2>
+                        <p className='text-white my-9 hidden lg:block md:block'>Break free from familiar scents. The fragrance of discovery. <br /> Share the sweet smell of success. A scent as soft as your skin. <br /> As famous perfumer Olivier Creed once said, your handbag may not <br /> be remembered, but your perfume will.</p>
+                        <button className="btn glass rounded-none normal-case my-4 lg:my-0 md:my-0 hover:bg-black">Discover More</button>
+                    </div>
+                    <img src={perfumeImage} alt="perfume" className='hidden lg:block' />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
