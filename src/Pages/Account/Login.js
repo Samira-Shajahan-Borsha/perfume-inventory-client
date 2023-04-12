2450c1986988e94818a3ed1aac1c8c3bc66a8a6e
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-04-13 01:58:10 +0600

    password type change in login form

--- src/Pages/Account/Login.js
+++ src/Pages/Account/Login.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Login = ({ handleLogin, handleEmailBlur, loginHandlePasswordBlur, handleForgetPassword }) => {
+
+    const [passwordVisible, setPasswordVisible] = useState(true);
 
     return (
         < div className="lg:w-1/2 w-full lg:mr-8">
@@ -13,11 +16,24 @@ const Login = ({ handleLogin, handleEmailBlur, loginHandlePasswordBlur, handleFo
                     </label>
                     <input onBlur={event => handleEmailBlur(event.target.value)} name='email' type="email" placeholder="Email" className="input input-bordered rounded text-sm" required />
                 </div>
-                <div className="form-control mt-3">
+                <div className="form-control mt-3 relative">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input onBlur={event => loginHandlePasswordBlur(event.target.value)} name='password' type="password" placeholder="Password" className="input input-bordered rounded text-sm" />
+                    <input
+                        onBlur={event => loginHandlePasswordBlur(event.target.value)}
+                        name='password'
+                        type={passwordVisible ? "password" : "text"}
+                        placeholder="Password"
+                        className="input input-bordered rounded text-sm"
+                    />
+                    <button
+                        onClick={() => setPasswordVisible(!passwordVisible)}
+                        className='absolute top-[52px] right-8'>
+                        {
+                            !passwordVisible ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+                        }
+                    </button>
                     <label className="label">
                         <Link className="label-text-alt link link-hover"><span onClick={handleForgetPassword} className='hover:text-yellow-700'>Forgot password?</span></Link>
                     </label>
